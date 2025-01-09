77357f91d7317e83d68f109c9392057d1e557a9e
vint10tov <viktordesiatov@mail.ru>
2025-01-09 17:50:19 +0600

    0

---
+++ build/CMakeFiles/ServerVintCrow.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/viktor/Рабочий стол/CrowSite/src/Source/main.cpp" "CMakeFiles/ServerVintCrow.dir/src/Source/main.cpp.o" "gcc" "CMakeFiles/ServerVintCrow.dir/src/Source/main.cpp.o.d"
+  "/home/viktor/Рабочий стол/CrowSite/src/Source/parser_body_relay.cpp" "CMakeFiles/ServerVintCrow.dir/src/Source/parser_body_relay.cpp.o" "gcc" "CMakeFiles/ServerVintCrow.dir/src/Source/parser_body_relay.cpp.o.d"
+  "/home/viktor/Рабочий стол/CrowSite/src/Source/request_from_server.cpp" "CMakeFiles/ServerVintCrow.dir/src/Source/request_from_server.cpp.o" "gcc" "CMakeFiles/ServerVintCrow.dir/src/Source/request_from_server.cpp.o.d"
+  "/home/viktor/Рабочий стол/CrowSite/src/Source/smart_home.cpp" "CMakeFiles/ServerVintCrow.dir/src/Source/smart_home.cpp.o" "gcc" "CMakeFiles/ServerVintCrow.dir/src/Source/smart_home.cpp.o.d"
+  "/home/viktor/Рабочий стол/CrowSite/src/Source/template_relay.cpp" "CMakeFiles/ServerVintCrow.dir/src/Source/template_relay.cpp.o" "gcc" "CMakeFiles/ServerVintCrow.dir/src/Source/template_relay.cpp.o.d"
+  "/home/viktor/Рабочий стол/CrowSite/src/Source/uart.cpp" "CMakeFiles/ServerVintCrow.dir/src/Source/uart.cpp.o" "gcc" "CMakeFiles/ServerVintCrow.dir/src/Source/uart.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/Source/main.cpp
@@ -0,0 +1,111 @@
+#include <crow.h>
+#include <unordered_map>
+#include <sstream>
+#include <string>
+
+#include "uart.hpp"
+//https://crowcpp.org/master/getting_started/a_simple_webpage/
+
+bool isInteger(const std::string& str);
+std::unordered_map<std::string, int> parseKeyValueString(const std::string& input);
+
+int main()
+{
+    crow::SimpleApp app;
+
+    // Статический маршрут для конкретного файла
+    CROW_ROUTE(app, "/css/<string>")
+    ([](const std::string& filepath) {
+        crow::response res;
+        res.set_static_file_info("static/" + filepath);  // Указать путь к файлу
+        return res;
+    });
+
+    // Определяем маршрут для главной страницы
+    CROW_ROUTE(app, "/")([](){
+        auto page = crow::mustache::load("index.html");
+
+        // Создаем контекст (JSON-объект)
+        crow::mustache::context ctx;
+        ctx["title"] = "Главная страница";
+
+        return page.render(ctx);
+    });
+
+    // Определяем маршрут для главной страницы
+    CROW_ROUTE(app, "/relay").methods(crow::HTTPMethod::GET, crow::HTTPMethod::POST)(
+        [](const crow::request &req){
+        auto page = crow::mustache::load("relay.html");
+
+        // Создаем контекст (JSON-объект)
+        crow::mustache::context ctx;
+        ctx["title"] = "Управление Реле";
+        ctx["error"] = "error ????????????????????";
+        ctx["time"] = "00:00:00/0/0/2000";
+        ctx["timeR2M0ON"] = "00:00:00";
+        ctx["timeR2M0OFF"] = "00:00:00";
+        ctx["timeR2M1ON"] = "00:00:00";
+        ctx["timeR2M1OFF"] = "00:00:00";
+        ctx["statusR0"] = "ON";
+        ctx["RstatusR0"] = "1";
+        ctx["statusR1"] = "ON";
+        ctx["RstatusR1"] = "1";
+        ctx["statusR2"] = "ON";
+        ctx["RstatusR2"] = "1";
+        ctx["statusR2M0"] = "ACT";
+        ctx["RstatusR2M0"] = "1";
+        ctx["statusR2M1"] = "ACT";
+        ctx["RstatusR2M1"] = "1";
+
+        if (req.method == crow::HTTPMethod::POST) {
+            auto result = parseKeyValueString(req.body);
+            // Вывод результата
+            for (const auto& pair : result) {
+                std::cout << pair.first << " = " << pair.second << std::endl;
+            }
+        }
+
+        return page.render(ctx);
+    });
+
+    // Запускаем сервер на порту 
+    app.port(8000)
+    .multithreaded()
+    .run();
+}
+
+bool isInteger(const std::string& str) {
+    if (str.empty()) return false;
+    for (char c : str) {
+        if (!std::isdigit(c)) return false;
+    }
+    return true;
+}
+
+std::unordered_map<std::string, int> parseKeyValueString(const std::string& input) {
+    std::unordered_map<std::string, int> result;
+    std::istringstream stream(input);
+    std::string pair;
+
+    while (std::getline(stream, pair, '&')) {
+        std::string key;
+        std::string value;
+
+        size_t equalsPos = pair.find('=');
+        if (equalsPos != std::string::npos) {
+            key = pair.substr(0, equalsPos);
+            value = pair.substr(equalsPos + 1);
+        } else {
+            key = pair;
+            value = "";
+        }
+
+        if (isInteger(value)) {
+            result[key] = std::stoi(value);
+        } else {
+            result[key] = 0;
+        }
+    }
+
+    return result;
+}
